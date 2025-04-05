a7dd66a5e6ff88a55dc7dbe45e7cbb1e26de7498
mobaibai <puresilent@qq.com>
2025-04-05 20:08:14 +0800

    🐞 fix: 解决cdn加载hdr报错问题

--- src/pages/three/index.jsx
+++ src/pages/three/index.jsx
@@ -1,10 +1,9 @@
 import { useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { useGLTF, OrbitControls, Stage, CameraShake, useAnimations } from '@react-three/drei'
+import { useGLTF, OrbitControls, Stage, CameraShake, useAnimations, useEnvironment, Environment } from '@react-three/drei'
 import { useTitle } from '@/hooks/useTitle'
+import HdrFile from '@/assets/hdr/potsdamer_platz_1k.hdr'
 import RobotModel from '@/assets/models/robot.glb'
-
-useGLTF.preload(RobotModel)
 
 function Model(props) {
   const { scene, animations } = useGLTF(RobotModel)
@@ -18,17 +17,25 @@ function Model(props) {
   return <primitive object={scene} {...props} />
 }
 
+function SceneContent() {
+  return (
+    <Stage environment={null} contactShadow={{ opacity: 1, blur: 2 }}>
+      {/* environment={null} 禁用环境贴图，并加载本地hdr，解决cdn加载报错问题 */}
+      <Environment files={HdrFile} />
+      <Model />
+    </Stage>
+  )
+}
+
 export const ThreePage = props => {
   if (props.title) {
     useTitle(props.title)
   }
 
   return (
-    <div className='three-container w-screen h-200 p-20'>
-      <Canvas shadows camera={{ fov: 50 }} frameloop='demand'>
-        <Stage contactShadow={{ opacity: 1, blur: 2 }}>
-          <Model />
-        </Stage>
+    <div className='three-container w-screen h-200 p-20 mx-auto'>
+      <Canvas shadows camera={{ fov: 60 }}>
+        <SceneContent />
         <OrbitControls makeDefault />
         <CameraShake
           maxYaw={0.1} // Max amount camera can yaw in either direction
@@ -38,11 +45,13 @@ export const ThreePage = props => {
           pitchFrequency={0.1} // Frequency of the pitch rotation
           rollFrequency={0.1} // Frequency of the roll rotation
           intensity={1} // initial intensity of the shake
-          decayRate={0.65} // if decay = true this is the rate at which intensity will reduce at />
+          decayRate={0.65} // if decay = true this is the rate at which intensity will reduce at
         />
       </Canvas>
     </div>
   )
 }
+
+useGLTF.preload(RobotModel)
 
 export default ThreePage
